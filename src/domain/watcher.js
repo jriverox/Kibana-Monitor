79d5d169353185e318e9ef43e7ae740a0df1b3fd
ipndeveloper <i_nav_31_8@hotmail.com>
2020-05-19 19:40:36 -0500

    Agregar validación para envio de notificaciones.

--- src/domain/watcher.js
+++ src/domain/watcher.js
@@ -13,8 +13,32 @@ module.exports = class Watcher {
       throw new Error(`No se ha encontrado la aplicacion: ${applicationName} en la configuracion`)
     this.notificationManager = new NotificationManager(this.applicationConfig)
   }
+  getRangeHour(){
+    var defaultHourStart ,defaultHourEnd,regex ,configHourStart , configHourEnd , hourStar , HourEnd;
+    defaultHourStart ="6:0";
+    defaultHourEnd ="20:0";
+    configHourStart =this.applicationConfig.HOURSTART;
+    configHourEnd=this.applicationConfig.HOUREND;
+    regex =/\b(2[0-3]|[01]?[0-9]):([0-5]?[0-9])\b/;
+     if(configHourStart && !regex.test(configHourStart))
+       configHourStart=defaultHourStart;
+     if(configHourEnd && !regex.test(configHourEnd))
+       configHourEnd=defaultHourEnd;
 
+       hourStar =  configHourStart.split(":");
+       HourEnd =  configHourEnd.split(":");
+       var fechaIni = new Date();
+       var fechaFin = new Date();
+       
+       fechaIni.setHours(hourStar[0] ,hourStar[1] ,0 ,0);
+       fechaFin.setHours(HourEnd[0] ,HourEnd[1],0 ,0);
+
+      return { hourStar : fechaIni , HourEnd  : fechaFin }
+  }
   async verify() {
+    var today  , rangeHour ;
+    today= new Date();
+    rangeHour =this.getRangeHour();
     const repository = new LogRepository(this.applicationConfig)
     const data = await repository.getData()
     console.log(data.aggregations.application.buckets)
@@ -26,8 +50,12 @@ module.exports = class Watcher {
       // notificar por supera el umbral
       for (const bucket of evaluatedBuckets) {
         const message = `Bucket: ${bucket.key} supera el umbral, valor: ${bucket.doc_count}`
-        console.log(message)
-        await this.notify(message)
+        console.log(message);
+        if(today >=rangeHour.hourStar  && today < rangeHour.HourEnd )
+           await this.notify(message)
+        else
+        console.log('Not Notification ');
+ 
       }
     }
   }
